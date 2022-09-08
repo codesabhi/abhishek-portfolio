95c29875ee7d994025613e407425ad156cb329d9
Abhishek <yadavabhishek241296@gmail.com>
2022-09-08 16:57:30 +0530

    changes updated

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -19,21 +19,21 @@ export default function Testimonial() {
         infiniteLoop={true}
         showThumbs={false}
         showStatus={false}
-        autoPlay={false}
+        autoPlay={true}
         interval={5000}
         >
                 <div className='testDivOne'>
-                    <p className='testdescOne' style={darkMode ? {} : {color: 'white'}}>It was great working with Abhishek. He's super proactive & passionate about everything he does. He picked up the brief quickly & was able to deliver flawlessly. Thanks, Abhishek!</p>
+                    <p className='testdescOne testAll' style={darkMode ? {} : {color: 'white'}}>It was great working with Abhishek. He's super proactive & passionate about everything he does. He picked up the brief quickly & was able to deliver flawlessly. Thanks, Abhishek!</p>
                     <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Anuj Lohiya</h2>
                     <p className='testProfile'>Founder, NG Greens</p>
                 </div>
                 <div className='testDivOne'>
-                    <p className='testdescTwo' style={darkMode ? {} : {color: 'white'}}>Abhishek is one of the most dedicated developer I’ve worked with and is willing to put that extra help whenever you need it. His expertise as a developer is considerable. I highly recommend Abhishek and would love to work with him again. </p>
+                    <p className='testdescTwo testAll' style={darkMode ? {} : {color: 'white'}}>Abhishek is one of the most dedicated developer I’ve worked with and is willing to put that extra help whenever you need it. His expertise as a developer is considerable. I highly recommend Abhishek and would love to work with him again. </p>
                     <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Sonali Verma</h2>
                     <p className='testProfile'>Analyst, Capgemini</p>
                 </div>
                 <div className='testDivOne'>
-                    <p className='testdescOne' style={darkMode ? {} : {color: 'white'}}>Abhishek is an amazing web developer and we will definitely be doing more projects in future. Great code, super smooth communication. We highly recommend Abhishek & would work with him again.</p>
+                    <p className='testdescOne testAll' style={darkMode ? {} : {color: 'white'}}>Abhishek is an amazing web developer and we will definitely be doing more projects in future. Great code, super smooth communication. We highly recommend Abhishek & would work with him again.</p>
                     <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Aditya Narayan</h2>
                     <p className='testProfile'>Founder, Stockcell</p>
                 </div>

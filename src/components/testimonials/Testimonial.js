70141e0db3c3d6c674b8afb85054e4e8e6525a7b
Abhishek <yadavabhishek241296@gmail.com>
2022-10-16 16:07:10 +0530

    update data

--- src/components/testimonials/Testimonial.js
+++ src/components/testimonials/Testimonial.js
@@ -27,6 +27,18 @@ export default function Testimonial() {
                     <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Anuj Lohiya</h2>
                     <p className='testProfile'>Founder, NG Greens</p>
                 </div>
+
+                <div className='testDivOne'>
+                    <p className='testdescOne testAll' style={darkMode ? {} : {color: 'white'}}>Our old website was terrible. I don't think it ever generated a lead for our sales team. Abhishek built a new site from the ground up, and we're extremely happy with it. Within 90 days we've generated  45+ new buyers and added $20 million in revenue.</p>
+                    <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Jose Cardoso</h2>
+                    <p className='testProfile'>CEO, Vanguard Properties</p>
+                </div>
+
+                <div className='testDivOne'>
+                    <p className='testdescOne testAll' style={darkMode ? {} : {color: 'white'}}>Abhishek is brilliant, he designed a high-converting website to setup my roofing and construction business. He successfully delivered the results and in just 60 days we started getting high-qualified leads and closed an extra 5 roofs per week. </p>
+                    <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Brandon Losik</h2>
+                    <p className='testProfile'>CEO, The Roof Commander</p>
+                </div>
                 <div className='testDivOne'>
                     <p className='testdescTwo testAll' style={darkMode ? {} : {color: 'white'}}>Abhishek is one of the most dedicated developer I’ve worked with and is willing to put that extra help whenever you need it. His expertise as a developer is considerable. I highly recommend Abhishek and would love to work with him again. </p>
                     <h2 className='testOwner' style={darkMode ? {} : {color: 'white'}}>Sonali Verma</h2>
